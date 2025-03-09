8b80592781e55acfa1443adf74cfd1194a7ea68f
BuYaoZaiKongFuShiChiZaoCan <103306275+BuYaoZaiKongFuShiChiZaoCan@users.noreply.github.com>
2025-03-09 09:38:50 +0800

    39 v6

--- js/audioMv.js
+++ js/audioMv.js
@@ -79,8 +79,6 @@ function audioMvmain(musicInfo = findMusic(title.textContent), autoplay = true) 
         // setTimeout(() => {
         video.webkitRequestFullScreen();
         // }, 17);
-        // 设置视频进度为音乐进度
-        video.currentTime = audio.currentTime;
         video.play();// 页面首次播放会提示需要用户点击一次才能自动播放 main.js:1  Uncaught (in promise) NotAllowedError: play() failed because the user didn't interact with the document first.
         video.onended = function () {
             lastChange === "prevSong" ? prevSong() : nextSong();
